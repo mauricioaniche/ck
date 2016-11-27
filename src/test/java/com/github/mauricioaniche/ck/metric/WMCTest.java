9f0677017526bcded40ed4a295891bd918cb1acb
Maurício Aniche <mauricioaniche@gmail.com>
2016-11-27 20:55:49 +0100

    refactored wmc tests

--- src/test/java/com/github/mauricioaniche/ck/metric/WMCTest.java
+++ src/test/java/com/github/mauricioaniche/ck/metric/WMCTest.java
@@ -1,6 +1,7 @@
 package com.github.mauricioaniche.ck.metric;
 
 import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import com.github.mauricioaniche.ck.CK;
@@ -9,11 +10,16 @@ import com.github.mauricioaniche.ck.CKReport;
 
 public class WMCTest extends BaseTest {
 
-	@Test
-	public void should_detect_wmc() {
-		
-		CKReport report = new CK().calculate(fixturesDir());
+	private static CKReport report;
+	
+	@BeforeClass
+	public static void setUp() {
+		report = new CK().calculate(fixturesDir() + "/wmc");
+	}
 
+	@Test
+	public void countAllBranchInstructions() {
+		
 		CKNumber a = report.getByClassName("wmc.CC1");
 		Assert.assertEquals(5, a.getWmc());
 		CKNumber b = report.getByClassName("wmc.CC2");
