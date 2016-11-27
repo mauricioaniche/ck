2bacfcc500e5c19c59e8199e0763efca7bf38ae6
Maurício Aniche <mauricioaniche@gmail.com>
2016-11-27 20:57:31 +0100

    refactored NOC

--- src/test/java/com/github/mauricioaniche/ck/metric/NOCTest.java
+++ src/test/java/com/github/mauricioaniche/ck/metric/NOCTest.java
@@ -1,6 +1,7 @@
 package com.github.mauricioaniche.ck.metric;
 
 import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import com.github.mauricioaniche.ck.CK;
@@ -9,16 +10,23 @@ import com.github.mauricioaniche.ck.CKReport;
 
 public class NOCTest extends BaseTest {
 
+	private static CKReport report;
+	
+	@BeforeClass
+	public static void setUp() {
+		report = new CK().calculate(fixturesDir() + "/dit");
+	}
+	
 	@Test
 	public void should_detect_children() {
 		
-		CKReport report = new CK().calculate(fixturesDir());
-
 		CKNumber a = report.getByClassName("dit.A");
 		Assert.assertEquals(1, a.getNoc());
 		CKNumber b = report.getByClassName("dit.B");
 		Assert.assertEquals(2, b.getNoc());
 		CKNumber c = report.getByClassName("dit.C");
-		Assert.assertEquals(0, c.getNoc());
+		Assert.assertEquals(1, c.getNoc());
+		CKNumber d = report.getByClassName("dit.D");
+		Assert.assertEquals(0, d.getNoc());
 	}
 }
