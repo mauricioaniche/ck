bc779c53b671367e1de26fe5fffc800bbbf07d36
Mauricio Aniche <mauricioaniche@gmail.com>
2019-03-07 16:11:37 +0100

    math operations test

---
+++ fixtures/math/Math.java
@@ -0,0 +1,19 @@
+package math;
+
+class Math {
+	
+	public void m1() {
+		int a = 10 + 1;
+		int b = 10 - 1;
+	}
+
+	public void m2() {
+		int c = 10 / 2;
+		int d = 10 * 2;
+		int e = 10 % 2;
+	}
+
+	public void m3() {
+		
+	}
+}
---
+++ src/test/java/com/github/mauricioaniche/ck/metric/NumberOfMathOperationsTest.java
@@ -0,0 +1,30 @@
+package com.github.mauricioaniche.ck.metric;
+
+import com.github.mauricioaniche.ck.CKClassResult;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.util.Map;
+
+public class NumberOfMathOperationsTest extends BaseTest {
+
+	private static Map<String, CKClassResult> report;
+
+	@BeforeClass
+	public static void setUp() {
+		report = run(fixturesDir() + "/math");
+	}
+
+	@Test
+	public void countLoops() {
+		CKClassResult a = report.get("math.Math");
+
+		Assert.assertEquals(5, a.getMathOperationsQty());
+
+		Assert.assertEquals(2, a.getMethod("m1/0").get().getMathOperationsQty());
+		Assert.assertEquals(3, a.getMethod("m2/0").get().getMathOperationsQty());
+		Assert.assertEquals(0, a.getMethod("m3/0").get().getMathOperationsQty());
+
+	}
+}
