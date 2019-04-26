a91dce4f395b484f3ec6aed37707f8c8b5afeab3
Mauricio Aniche <mauricioaniche@gmail.com>
2019-04-26 23:05:28 +0200

    test for the exception we throw due to some crazy generics (and thus, repeated methods)

---
+++ fixtures/badgenerics/BG.java
@@ -0,0 +1,16 @@
+package bg;
+
+import java.util.Map;
+import java.util.List;
+
+class BG {
+
+
+	public static <T extends Map<?, ?>> T notEmpty(final T map, final String message, final Object... values) {
+		return null;
+	}
+
+	public static <T extends List<?, ?>> T notEmpty(final T map, final String message, final Object... values) {
+		return null;
+	}
+}
