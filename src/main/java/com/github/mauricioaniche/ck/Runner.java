04350c0841de8e1947159c2cbf571e3d5568ff91
Ayaan Kazerouni <ayaan@vt.edu>
2019-06-26 11:42:51 -0400

    Use Apache commons CSVPrinter for output

--- src/main/java/com/github/mauricioaniche/ck/Runner.java
+++ src/main/java/com/github/mauricioaniche/ck/Runner.java
@@ -1,143 +1,93 @@
 package com.github.mauricioaniche.ck;
 
-import java.io.FileNotFoundException;
-import java.io.PrintStream;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Map;
-import java.util.Set;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVPrinter;
 
 public class Runner {
 
-	public static void main(String[] args) throws FileNotFoundException {
-		
-		if(args==null || args.length != 1) {
+	public static void main(String[] args) {
+
+		if (args == null || args.length != 1) {
 			System.out.println("Usage java -jar ck.jar <path to project>");
 			System.exit(1);
 		}
-		
+
 		String path = args[0];
 
-		PrintStream classOutput = new PrintStream("class.csv");
-		classOutput.println("file,class,type,cbo,wmc,dit,rfc,lcom,totalMethods,staticMethods,publicMethods,privateMethods,protectedMethods," +
-				"defaultMethods,abstractMethods,finalMethods,synchronizedMethods,totalFields,staticFields,publicFields,privateFields," +
-				"protectedFields,defaultFields,finalFields,synchronizedFields,nosi,loc,returnQty,loopQty,comparisonsQty," +
-				"tryCatchQty,parenthesizedExpsQty,stringLiteralsQty,numbersQty,assignmentsQty,mathOperationsQty,variablesQty," +
-				"maxNestedBlocks,anonymousClassesQty,subClassesQty,lambdasQty,uniqueWordsQty");
-
-		PrintStream methodOutput = new PrintStream("method.csv");
-		methodOutput.println("file,class,method,line,cbo,wmc,rfc,loc,returns,variables,parameters,startLine," +
-				"loopQty,comparisonsQty,tryCatchQty,parenthesizedExpsQty,stringLiteralsQty,numbersQty,assignmentsQty," +
-				"mathOperationsQty,maxNestedBlocks,anonymousClassesQty,subClassesQty,lambdasQty,uniqueWordsQty");
-
-		PrintStream variableOutput = new PrintStream("variable.csv");
-		variableOutput.println("file,class,method,variable,usage");
-
-		PrintStream fieldOutput = new PrintStream("field.csv");
-		fieldOutput.println("file,class,method,variable,usage");
-
 		new CK().calculate(path, result -> {
-			if(result.isError()) return;
+			try {
+				writeResults(result);
+			} catch (IOException e) {
+				throw new RuntimeException(e);
+			}
+		});
+	}
 
-			classOutput.println(
-				result.getFile() + "," +
-				result.getClassName() + "," +
-				result.getType() + "," +
-				result.getCbo() + "," +
-				result.getWmc() + "," +
-				result.getDit() + "," +
-				result.getRfc() + "," +
-				result.getLcom() + "," +
-				result.getNumberOfMethods() + "," +
-				result.getNumberOfStaticMethods() + "," +
-				result.getNumberOfPublicMethods() + "," +
-				result.getNumberOfPrivateMethods() + "," +
-				result.getNumberOfProtectedMethods() + "," +
-				result.getNumberOfDefaultMethods() + "," +
-				result.getNumberOfAbstractMethods() + "," +
-				result.getNumberOfFinalMethods() + "," +
-				result.getNumberOfSynchronizedMethods() + "," +
-				result.getNumberOfFields() + "," +
-				result.getNumberOfStaticFields() + "," +
-				result.getNumberOfPublicFields() + "," +
-				result.getNumberOfPrivateFields() + "," +
-				result.getNumberOfProtectedFields() + "," +
-				result.getNumberOfDefaultFields() + "," +
-				result.getNumberOfFinalFields() + "," +
-				result.getNumberOfSynchronizedFields() + "," +
-				result.getNosi() + "," +
-				result.getLoc() + "," +
-				result.getReturnQty() + "," +
-				result.getLoopQty() + "," +
-				result.getComparisonsQty() + "," +
-				result.getTryCatchQty() + "," +
-				result.getParenthesizedExpsQty() + "," +
-				result.getStringLiteralsQty() + "," +
-				result.getNumbersQty() + "," +
-				result.getAssignmentsQty() + "," +
-				result.getMathOperationsQty() + "," +
-				result.getMathOperationsQty() + "," +
-				result.getVariablesQty() + "," +
-				result.getMaxNestedBlocks() + "," +
-				result.getAnonymousClassesQty() + "," +
-				result.getSubClassesQty() + "," +
-				result.getLambdasQty() + "," +
-				result.getUniqueWordsQty()
-			);
+	public static void writeResults(CKClassResult result) throws IOException {
+		if (result.isError())
+			return;
 
-			for(CKMethodResult method : result.getMethods()) {
-				methodOutput.println(
-					result.getFile() + "," +
-					result.getClassName() + "," +
-					method.getMethodName() + "," +
-					method.getStartLine() + "," +
-					method.getCbo() + "," +
-					method.getWmc() + "," +
-					method.getRfc() + "," +
-					method.getLoc() + "," +
-					method.getReturnQty() + "," +
-					method.getVariablesQty() + "," +
-					method.getParametersQty()  + "," +
-					method.getStartLine() + "," +
-					method.getLoopQty() + "," +
-					method.getComparisonsQty() + "," +
-					method.getTryCatchQty() + "," +
-					method.getParenthesizedExpsQty() + "," +
-					method.getStringLiteralsQty() + "," +
-					method.getNumbersQty() + "," +
-					method.getAssignmentsQty() + "," +
-					method.getMathOperationsQty() + "," +
-					method.getMaxNestedBlocks() + "," +
-					method.getAnonymousClassesQty() + "," +
-					method.getSubClassesQty() + "," +
-					method.getLambdasQty() + "," +
-					method.getUniqueWordsQty()
-				);
+		String[] classHeader = { "file", "class", "type", "cbo", "wmc", "dit", "rfc", "lcom", "totalMethods",
+				"staticMethods", "publicMethods", "privateMethods", "protectedMethods", "defaultMethods",
+				"abstractMethods", "finalMethods", "synchronizedMethods", "totalFields", "staticFields", "publicFields",
+				"privateFields", "protectedFields", "defaultFields", "finalFields", "synchronizedFields", "nosi", "loc",
+				"returnQty", "loopQty", "comparisonsQty", "tryCatchQty", "parenthesizedExpsQty", "stringLiteralsQty",
+				"numbersQty", "assignmentsQty", "mathOperationsQty", "variablesQty", "maxNestedBlocks",
+				"anonymousClassesQty", "subClassesQty", "lambdasQty", "uniqueWordsQty" };
+		String[] methodHeader = { "file", "class", "method", "line", "cbo", "wmc", "rfc", "loc", "returns", "variables",
+				"parameters", "startLine", "loopQty", "comparisonsQty", "tryCatchQty", "parenthesizedExpsQty",
+				"stringLiteralsQty", "numbersQty", "assignmentsQty", "mathOperationsQty", "maxNestedBlocks",
+				"anonymousClassesQty", "subClassesQty", "lambdasQty", "uniqueWordsQty" };
+		String[] varFieldHeader = { "file", "class", "method", "variable", "usage" };
 
-				for(Map.Entry<String,Integer> entry : method.getVariablesUsage().entrySet()) {
+		try (FileWriter classOutput = new FileWriter("class.csv");
+				CSVPrinter classPrinter = new CSVPrinter(classOutput, CSVFormat.DEFAULT.withHeader(classHeader));
+				FileWriter methodOut = new FileWriter("method.csv");
+				CSVPrinter methodPrinter = new CSVPrinter(methodOut, CSVFormat.DEFAULT.withHeader(methodHeader));
+				FileWriter variableOut = new FileWriter("variable.csv");
+				CSVPrinter variablePrinter = new CSVPrinter(variableOut, CSVFormat.DEFAULT.withHeader(varFieldHeader));
+				FileWriter fieldOut = new FileWriter("field.csv");
+				CSVPrinter fieldPrinter = new CSVPrinter(fieldOut, CSVFormat.DEFAULT.withHeader(varFieldHeader))) {
+			classPrinter.printRecord(result.getFile(), result.getClassName(), result.getType(), result.getCbo(),
+					result.getWmc(), result.getDit(), result.getRfc(), result.getLcom(), result.getNumberOfMethods(),
+					result.getNumberOfStaticMethods(), result.getNumberOfPublicMethods(),
+					result.getNumberOfPrivateMethods(), result.getNumberOfProtectedMethods(),
+					result.getNumberOfDefaultMethods(), result.getNumberOfAbstractMethods(),
+					result.getNumberOfFinalMethods(), result.getNumberOfSynchronizedMethods(),
+					result.getNumberOfFields(), result.getNumberOfStaticFields(), result.getNumberOfPublicFields(),
+					result.getNumberOfPrivateFields(), result.getNumberOfProtectedFields(),
+					result.getNumberOfDefaultFields(), result.getNumberOfFinalFields(),
+					result.getNumberOfSynchronizedFields(), result.getNosi(), result.getLoc(), result.getReturnQty(),
+					result.getLoopQty(), result.getComparisonsQty(), result.getTryCatchQty(),
+					result.getParenthesizedExpsQty(), result.getStringLiteralsQty(), result.getNumbersQty(),
+					result.getAssignmentsQty(), result.getMathOperationsQty(), result.getVariablesQty(),
+					result.getMaxNestedBlocks(), result.getAnonymousClassesQty(), result.getSubClassesQty(),
+					result.getLambdasQty(), result.getUniqueWordsQty());
 
-					variableOutput.println(
-						result.getFile() + "," +
-						result.getClassName() + "," +
-						method.getMethodName() + "," +
-						entry.getKey() + "," + entry.getValue());
+			for (CKMethodResult method : result.getMethods()) {
+				methodPrinter.printRecord(result.getFile(), result.getClassName(), method.getMethodName(),
+						method.getStartLine(), method.getCbo(), method.getWmc(), method.getRfc(), method.getLoc(),
+						method.getReturnQty(), method.getVariablesQty(), method.getParametersQty(),
+						method.getStartLine(), method.getLoopQty(), method.getComparisonsQty(), method.getTryCatchQty(),
+						method.getParenthesizedExpsQty(), method.getStringLiteralsQty(), method.getNumbersQty(),
+						method.getAssignmentsQty(), method.getMathOperationsQty(), method.getMaxNestedBlocks(),
+						method.getAnonymousClassesQty(), method.getSubClassesQty(), method.getLambdasQty(),
+						method.getUniqueWordsQty());
+
+				for (Map.Entry<String, Integer> entry : method.getVariablesUsage().entrySet()) {
+					variablePrinter.printRecord(result.getFile(), result.getClassName(), method.getMethodName(),
+							entry.getKey(), entry.getValue());
 				}
 
-				for(Map.Entry<String,Integer> entry : method.getFieldUsage().entrySet()) {
-
-					fieldOutput.println(
-						result.getFile() + "," +
-						result.getClassName() + "," +
-						method.getMethodName() + "," +
-						entry.getKey() + "," + entry.getValue());
+				for (Map.Entry<String, Integer> entry : method.getFieldUsage().entrySet()) {
+					fieldPrinter.printRecord(result.getFile(), result.getClassName(), method.getMethodName(),
+							entry.getKey(), entry.getValue());
 				}
 			}
-
-
-		});
-		
-		classOutput.close();
-		methodOutput.close();
-		variableOutput.close();
-		fieldOutput.close();
-		
+		}
 	}
 }
