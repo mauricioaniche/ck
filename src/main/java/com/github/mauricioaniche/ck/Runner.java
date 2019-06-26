cc7c471857dae09ed9efc9be8f3ee80c021236c6
Ayaan Kazerouni <ayaan@vt.edu>
2019-06-26 12:04:46 -0400

    Flush printers after obtaining all results

--- src/main/java/com/github/mauricioaniche/ck/Runner.java
+++ src/main/java/com/github/mauricioaniche/ck/Runner.java
@@ -9,7 +9,7 @@ import org.apache.commons.csv.CSVPrinter;
 
 public class Runner {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 
 		if (args == null || args.length != 1) {
 			System.out.println("Usage java -jar ck.jar <path to project>");
@@ -17,19 +17,6 @@ public class Runner {
 		}
 
 		String path = args[0];
-
-		new CK().calculate(path, result -> {
-			try {
-				writeResults(result);
-			} catch (IOException e) {
-				throw new RuntimeException(e);
-			}
-		});
-	}
-
-	public static void writeResults(CKClassResult result) throws IOException {
-		if (result.isError())
-			return;
 
 		String[] classHeader = { "file", "class", "type", "cbo", "wmc", "dit", "rfc", "lcom", "totalMethods",
 				"staticMethods", "publicMethods", "privateMethods", "protectedMethods", "defaultMethods",
@@ -43,51 +30,112 @@ public class Runner {
 				"stringLiteralsQty", "numbersQty", "assignmentsQty", "mathOperationsQty", "maxNestedBlocks",
 				"anonymousClassesQty", "subClassesQty", "lambdasQty", "uniqueWordsQty" };
 		String[] varFieldHeader = { "file", "class", "method", "variable", "usage" };
+		
+		FileWriter classOut = new FileWriter("class.csv");
+		CSVPrinter classPrinter = new CSVPrinter(classOut, CSVFormat.DEFAULT.withHeader(classHeader));
+		FileWriter methodOut = new FileWriter("method.csv");
+		CSVPrinter methodPrinter = new CSVPrinter(methodOut, CSVFormat.DEFAULT.withHeader(methodHeader));
+		FileWriter variableOut = new FileWriter("variable.csv");
+		CSVPrinter variablePrinter = new CSVPrinter(variableOut, CSVFormat.DEFAULT.withHeader(varFieldHeader));
+		FileWriter fieldOut = new FileWriter("field.csv");
+		CSVPrinter fieldPrinter = new CSVPrinter(fieldOut, CSVFormat.DEFAULT.withHeader(varFieldHeader));
+		
+		new CK().calculate(path, result -> {
+			try {
+				classPrinter.printRecord(
+						result.getFile(),
+						result.getClassName(),
+						result.getType(),
+						result.getCbo(),
+						result.getWmc(),
+						result.getDit(),
+						result.getRfc(),
+						result.getLcom(),
+						result.getNumberOfMethods(),
+						result.getNumberOfStaticMethods(),
+						result.getNumberOfPublicMethods(),
+						result.getNumberOfPrivateMethods(),
+						result.getNumberOfProtectedMethods(),
+						result.getNumberOfDefaultMethods(),
+						result.getNumberOfAbstractMethods(),
+						result.getNumberOfFinalMethods(),
+						result.getNumberOfSynchronizedMethods(),
+						result.getNumberOfFields(),
+						result.getNumberOfStaticFields(),
+						result.getNumberOfPublicFields(),
+						result.getNumberOfPrivateFields(),
+						result.getNumberOfProtectedFields(),
+						result.getNumberOfDefaultFields(),
+						result.getNumberOfFinalFields(),
+						result.getNumberOfSynchronizedFields(),
+						result.getNosi(),
+						result.getLoc(),
+						result.getReturnQty(),
+						result.getLoopQty(),
+						result.getComparisonsQty(),
+						result.getTryCatchQty(),
+						result.getParenthesizedExpsQty(),
+						result.getStringLiteralsQty(),
+						result.getNumbersQty(),
+						result.getAssignmentsQty(),
+						result.getMathOperationsQty(),
+						result.getVariablesQty(),
+						result.getMaxNestedBlocks(),
+						result.getAnonymousClassesQty(),
+						result.getSubClassesQty(),
+						result.getLambdasQty(),
+						result.getUniqueWordsQty());
 
-		try (FileWriter classOutput = new FileWriter("class.csv");
-				CSVPrinter classPrinter = new CSVPrinter(classOutput, CSVFormat.DEFAULT.withHeader(classHeader));
-				FileWriter methodOut = new FileWriter("method.csv");
-				CSVPrinter methodPrinter = new CSVPrinter(methodOut, CSVFormat.DEFAULT.withHeader(methodHeader));
-				FileWriter variableOut = new FileWriter("variable.csv");
-				CSVPrinter variablePrinter = new CSVPrinter(variableOut, CSVFormat.DEFAULT.withHeader(varFieldHeader));
-				FileWriter fieldOut = new FileWriter("field.csv");
-				CSVPrinter fieldPrinter = new CSVPrinter(fieldOut, CSVFormat.DEFAULT.withHeader(varFieldHeader))) {
-			classPrinter.printRecord(result.getFile(), result.getClassName(), result.getType(), result.getCbo(),
-					result.getWmc(), result.getDit(), result.getRfc(), result.getLcom(), result.getNumberOfMethods(),
-					result.getNumberOfStaticMethods(), result.getNumberOfPublicMethods(),
-					result.getNumberOfPrivateMethods(), result.getNumberOfProtectedMethods(),
-					result.getNumberOfDefaultMethods(), result.getNumberOfAbstractMethods(),
-					result.getNumberOfFinalMethods(), result.getNumberOfSynchronizedMethods(),
-					result.getNumberOfFields(), result.getNumberOfStaticFields(), result.getNumberOfPublicFields(),
-					result.getNumberOfPrivateFields(), result.getNumberOfProtectedFields(),
-					result.getNumberOfDefaultFields(), result.getNumberOfFinalFields(),
-					result.getNumberOfSynchronizedFields(), result.getNosi(), result.getLoc(), result.getReturnQty(),
-					result.getLoopQty(), result.getComparisonsQty(), result.getTryCatchQty(),
-					result.getParenthesizedExpsQty(), result.getStringLiteralsQty(), result.getNumbersQty(),
-					result.getAssignmentsQty(), result.getMathOperationsQty(), result.getVariablesQty(),
-					result.getMaxNestedBlocks(), result.getAnonymousClassesQty(), result.getSubClassesQty(),
-					result.getLambdasQty(), result.getUniqueWordsQty());
+				for (CKMethodResult method : result.getMethods()) {
+					methodPrinter.printRecord(
+							result.getFile(), 
+							result.getClassName(), 
+							method.getMethodName(),
+							method.getStartLine(),
+							method.getCbo(),
+							method.getWmc(),
+							method.getRfc(),
+							method.getLoc(),
+							method.getReturnQty(),
+							method.getVariablesQty(),
+							method.getParametersQty(),
+							method.getStartLine(),
+							method.getLoopQty(),
+							method.getComparisonsQty(),
+							method.getTryCatchQty(),
+							method.getParenthesizedExpsQty(),
+							method.getStringLiteralsQty(),
+							method.getNumbersQty(),
+							method.getAssignmentsQty(),
+							method.getMathOperationsQty(),
+							method.getMaxNestedBlocks(),
+							method.getAnonymousClassesQty(),
+							method.getSubClassesQty(),
+							method.getLambdasQty(),
+							method.getUniqueWordsQty());
 
-			for (CKMethodResult method : result.getMethods()) {
-				methodPrinter.printRecord(result.getFile(), result.getClassName(), method.getMethodName(),
-						method.getStartLine(), method.getCbo(), method.getWmc(), method.getRfc(), method.getLoc(),
-						method.getReturnQty(), method.getVariablesQty(), method.getParametersQty(),
-						method.getStartLine(), method.getLoopQty(), method.getComparisonsQty(), method.getTryCatchQty(),
-						method.getParenthesizedExpsQty(), method.getStringLiteralsQty(), method.getNumbersQty(),
-						method.getAssignmentsQty(), method.getMathOperationsQty(), method.getMaxNestedBlocks(),
-						method.getAnonymousClassesQty(), method.getSubClassesQty(), method.getLambdasQty(),
-						method.getUniqueWordsQty());
+					for (Map.Entry<String, Integer> entry : method.getVariablesUsage().entrySet()) {
+						variablePrinter.printRecord(result.getFile(), result.getClassName(), method.getMethodName(),
+								entry.getKey(), entry.getValue());
+					}
 
-				for (Map.Entry<String, Integer> entry : method.getVariablesUsage().entrySet()) {
-					variablePrinter.printRecord(result.getFile(), result.getClassName(), method.getMethodName(),
-							entry.getKey(), entry.getValue());
+					for (Map.Entry<String, Integer> entry : method.getFieldUsage().entrySet()) {
+						fieldPrinter.printRecord(result.getFile(), result.getClassName(), method.getMethodName(),
+								entry.getKey(), entry.getValue());
+					}
 				}
-
-				for (Map.Entry<String, Integer> entry : method.getFieldUsage().entrySet()) {
-					fieldPrinter.printRecord(result.getFile(), result.getClassName(), method.getMethodName(),
-							entry.getKey(), entry.getValue());
-				}
+			} catch (IOException e) {
+				throw new RuntimeException(e);
 			}
-		}
+		});
+		
+		classPrinter.flush();
+		classPrinter.close();
+		methodPrinter.flush();
+		methodPrinter.close();
+		variablePrinter.flush();
+		variablePrinter.close();
+		fieldPrinter.flush();
+		fieldPrinter.close();
 	}
 }
