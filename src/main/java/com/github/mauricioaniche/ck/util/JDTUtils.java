6c08c9fb4747cdc19a8deb1cf6e80e294f81c67b
Mauricio Aniche <mauricioaniche@gmail.com>
2019-11-03 16:14:05 +0100

    remove not used function

--- src/main/java/com/github/mauricioaniche/ck/util/JDTUtils.java
+++ src/main/java/com/github/mauricioaniche/ck/util/JDTUtils.java
@@ -7,39 +7,6 @@ import java.util.List;
 
 public class JDTUtils {
 
-//	public static String getMethodFullName(MethodDeclaration node) {
-//		String methodName = node.getName().getFullyQualifiedName();
-//
-//		int parameterCount = node.parameters()==null ? 0 : node.parameters().size();
-//		List<String> parameterTypes = new ArrayList<>();
-//
-//		if(parameterCount > 0) {
-//			for(Object p0 : node.parameters()) {
-//				SingleVariableDeclaration parameter = (SingleVariableDeclaration) p0;
-//
-//				ITypeBinding binding = parameter.getType().resolveBinding();
-//
-//				String v;
-//				if(binding == null || binding.isRecovered())
-//					v = parameter.getType().toString();
-//				else
-//					v = binding.getQualifiedName();
-//
-//				if(parameter.isVarargs()) v+="[]";
-//
-//				parameterTypes.add(v);
-//			}
-//		}
-//
-//		return String.format("%s/%d%s%s%s",
-//				methodName,
-//				parameterCount,
-//				(parameterCount > 0 ? "[" : ""),
-//				(parameterCount > 0 ? String.join(",", parameterTypes) : ""),
-//				(parameterCount > 0 ? "]" : "")
-//		);
-//	}
-
 	/**
 	 * If the method has a body, we can get the starting line of the method, ignoring any possible
 	 * Javadoc at the top of it.
