cfa9f3af000b6025d4fae4d6b09da7259c218330
Mauricio Aniche <mauricioaniche@gmail.com>
2020-01-22 11:39:10 +0100

    improving the test fixture

--- fixtures/class-types/A.java
+++ fixtures/class-types/A.java
@@ -8,14 +8,26 @@ public class A {
 	}
 
 	public void m2() {
-		new B() {{
-			void m2() {
 
+		Thread t = new Thread()
+		{
+			public void run()
+			{
+				System.out.println("Child Thread 1");
 			}
-		}}
+		};
 	}
 
 	public void m3() {
 		MathOperation addition = (int a, int b) -> a + b;
+
+		MathOperation op2 = new MathOperation() {
+			public int operation(int a, int b) {
+				return a + b;
+			}
+		};
+
+		int result = op2.operation(2, 2);
+
 	}
 }
--- src/test/java/com/github/mauricioaniche/ck/ClassTypeTest.java
+++ src/test/java/com/github/mauricioaniche/ck/ClassTypeTest.java
@@ -4,6 +4,8 @@ import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Map;
 
 public class ClassTypeTest extends BaseTest {
@@ -18,6 +20,9 @@ public class ClassTypeTest extends BaseTest {
 
 	@Test
 	public void identifyTypesCorrectly() {
+
+		Assert.assertEquals(5, report.size());
+
 		CKClassResult a = report.get("classtypes.A");
 		Assert.assertEquals("class", a.getType());
 
@@ -26,6 +31,13 @@ public class ClassTypeTest extends BaseTest {
 
 		CKClassResult mathOperation = report.get("classtypes.MathOperation");
 		Assert.assertEquals("interface", mathOperation.getType());
+
+		CKClassResult anon1 = report.get("classtypes.A$Anonymous1");
+		Assert.assertEquals("anonymous", anon1.getType());
+
+		CKClassResult anon2 = report.get("classtypes.A$Anonymous2");
+		Assert.assertEquals("anonymous", anon2.getType());
+
 
 	}
 
