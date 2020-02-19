507bae0f37207ed256c836a477724cfb19475d1d
David Bit <30724543+Taeon7@users.noreply.github.com>
2020-02-18 22:43:24 -0300

    Create Variables2.java
    Adding more types of variables.

---
+++ fixtures/variables/Variables2.java
@@ -0,0 +1,17 @@
+package variables;
+
+class Variables2 {
+	
+	public void m1() {
+
+        boolean typeBoolean = true;
+        byte typeByte = 357;
+        char typeChar = 'J'; 
+        double typeDouble = 3.57;
+        float typeFloat = 3.5f;
+        long typeLong = 35712357123571235712;
+        int typeInt = 3571235712;
+        short typeShort = 35712;
+        
+	}
+}
