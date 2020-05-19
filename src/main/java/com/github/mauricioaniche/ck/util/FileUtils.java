6eae711193d4b2ef87396cd7cb5c07445a0adacd
Jan Gerling <jan.gerling@posteo.de>
2020-05-19 12:11:39 +0200

    Fix #63 : CK not finding source files
     * Filter only git folder FilePath extraction
     * Extract method getAllFiles in FileUtils

--- src/main/java/com/github/mauricioaniche/ck/util/FileUtils.java
+++ src/main/java/com/github/mauricioaniche/ck/util/FileUtils.java
@@ -3,14 +3,13 @@ package com.github.mauricioaniche.ck.util;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
-
 public class FileUtils {
-
+	//Get all directories from the directory at the given path.
 	public static String[] getAllDirs(String path) {
 		try {
 			return Files.walk(Paths.get(path))
 					.filter(Files::isDirectory)
-					.filter(x -> !x.toAbsolutePath().toString().contains(".git"))
+					.filter(x -> !x.toAbsolutePath().toString().contains(".git/"))
 					.map(x -> x.toAbsolutePath().toString())
 					.toArray(String[]::new);
 		} catch(Exception e) {
@@ -18,34 +17,27 @@ public class FileUtils {
 		}
 	}
 
+	//Get all java class files from the directory at the given path.
 	public static String[] getAllJavaFiles(String path) {
-
-		try {
-			return Files.walk(Paths.get(path))
-					.filter(Files::isRegularFile)
-					.filter(x -> !x.toAbsolutePath().toString().contains(".git"))
-					.filter(x -> x.toAbsolutePath().toString().toLowerCase().endsWith("java"))
-					.map(x -> x.toAbsolutePath().toString())
-					.toArray(String[]::new);
-		} catch(Exception e) {
-			throw new RuntimeException(e);
-		}
-
+		return getAllFiles(path, "java");
 	}
 
+	//Get all jars from the directory at the given path.
 	public static String[] getAllJars(String path) {
+		return getAllFiles(path, "jar");
+	}
 
+	//Get all files from of the given file ending from the directory at the given path.
+	private static String[] getAllFiles(String path, String ending){
 		try {
 			return Files.walk(Paths.get(path))
 					.filter(Files::isRegularFile)
-					.filter(x -> !x.toAbsolutePath().toString().contains(".git"))
-					.filter(x -> x.toAbsolutePath().toString().toLowerCase().endsWith("jar"))
+					.filter(x -> !x.toAbsolutePath().toString().contains(".git/"))
+					.filter(x -> x.toAbsolutePath().toString().toLowerCase().endsWith(ending))
 					.map(x -> x.toAbsolutePath().toString())
 					.toArray(String[]::new);
 		} catch(Exception e) {
 			throw new RuntimeException(e);
 		}
 	}
-	
-	
 }
