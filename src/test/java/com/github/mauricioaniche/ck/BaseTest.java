d6dc8a646c279130771842531494e6c17f358760
Mauricio Aniche <mauricioaniche@gmail.com>
2020-05-24 12:13:43 +0200

    testing that results are never null or < 0

--- src/test/java/com/github/mauricioaniche/ck/BaseTest.java
+++ src/test/java/com/github/mauricioaniche/ck/BaseTest.java
@@ -2,15 +2,32 @@ package com.github.mauricioaniche.ck;
 
 import com.github.mauricioaniche.ck.metric.ClassLevelMetric;
 import com.github.mauricioaniche.ck.metric.MethodLevelMetric;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 import java.io.File;
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.*;
 import java.util.concurrent.Callable;
+import java.util.stream.Collectors;
 
 public abstract class BaseTest {
+
+
+	static Set<Method> classGetters = Arrays.stream(CKClassResult.class.getMethods())
+			.filter(m -> m.getName().startsWith("get"))
+			.filter(m -> !m.getName().equals("getModifiers")) // it can be -1 if it's anonymous
+			.filter(m -> m.getParameterCount() == 0)
+			.collect(Collectors.toSet());
+
+	static Set<Method> methodGetters = Arrays.stream(CKMethodResult.class.getMethods())
+			.filter(m -> m.getName().startsWith("get"))
+			.filter(m -> !m.getName().equals("getModifiers"))
+			.filter(m -> m.getParameterCount() == 0)
+			.collect(Collectors.toSet());
+
 
 	protected Map<String, CKClassResult> report;
 
@@ -23,9 +40,43 @@ public abstract class BaseTest {
 		}
 	}
 
-	@AfterAll
-	public void checkNulls() {
-		report = run(fixturesDir() + "/cbo");
+	@Test
+	public void checkNulls() throws InvocationTargetException, IllegalAccessException {
+		if(report==null)
+			return;
+
+		for(String reportedClass : report.keySet()) {
+			// no nulls in getters of the class
+			CKClassResult ckClassResult = report.get(reportedClass);
+			for(Method method : classGetters) {
+				Object result = method.invoke(ckClassResult);
+				Assertions.assertNotNull(result, method.getName() + " is null");
+
+				// integer >= 0
+				if(method.getReturnType().equals(int.class)) {
+					int intResult = (int) result;
+					Assertions.assertTrue(intResult >= 0, method.getName() + " < 0");
+				}
+			}
+
+			Set<CKMethodResult> ckMethods = ckClassResult.getMethods();
+			// no nulls in getters of the class
+			for (CKMethodResult ckMethodResult : ckMethods) {
+				for(Method method : methodGetters) {
+					Object result = method.invoke(ckMethodResult);
+					Assertions.assertNotNull(result, method.getName() + " is null");
+
+					// integer >= 0
+					if(method.getReturnType().equals(int.class)) {
+						int intResult = (int) result;
+						Assertions.assertTrue(intResult >= 0, method.getName() + " < 0");
+					}
+
+				}
+			}
+		}
+
+
 	}
 
 	protected static Map<String, CKClassResult> run(String dir) {
