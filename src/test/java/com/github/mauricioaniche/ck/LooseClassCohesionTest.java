e66a4a1979e9960d68575eef3320f93389dd63c3
David van der Leij <davidvanderleij@gmail.com>
2020-09-08 13:28:15 +0200

    * Remove pitest dependency. * Move assert statements used form transitive pitest to jupiter api * Change ArrayUtils usage from lang2 in pytest dependency to lang3 native dependency

--- src/test/java/com/github/mauricioaniche/ck/LooseClassCohesionTest.java
+++ src/test/java/com/github/mauricioaniche/ck/LooseClassCohesionTest.java
@@ -1,9 +1,11 @@
 package com.github.mauricioaniche.ck;
 
-import junit.framework.Assert;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.Map;
 
@@ -18,43 +20,43 @@ public class LooseClassCohesionTest extends BaseTest {
     @Test
     public void highCohesion() {
         CKClassResult ckClass = report.get("ClassCohesion.HighCohesion");
-        Assert.assertEquals(1f, ckClass.getLooseClassCohesion(),0.0000001);
+        assertEquals(1f, ckClass.getLooseClassCohesion(),0.0000001);
     }
 
     @Test
     public void noCohesion() {
         CKClassResult ckClass = report.get("ClassCohesion.NoCohesion");
-        Assert.assertEquals(0f, ckClass.getLooseClassCohesion(),0.0000001);
+        assertEquals(0f, ckClass.getLooseClassCohesion(),0.0000001);
     }
 
     @Test
     public void mediumCohesion1() {
         CKClassResult ckClass = report.get("ClassCohesion.MediumCohesion");
-        Assert.assertEquals(2f / 6f, ckClass.getLooseClassCohesion(),0.0000001);
+        assertEquals(2f / 6f, ckClass.getLooseClassCohesion(),0.0000001);
     }
 
     @Test
     public void mediumCohesion2() {
         CKClassResult ckClass = report.get("ClassCohesion.Simple1");
-        Assert.assertEquals(2f / 10f, ckClass.getLooseClassCohesion(),0.0000001);
+        assertEquals(2f / 10f, ckClass.getLooseClassCohesion(),0.0000001);
     }
 
     @Test
     public void mediumCohesion3() {
         CKClassResult ckClass = report.get("ClassCohesion.Simple2");
-        Assert.assertEquals(6f / 10f, ckClass.getLooseClassCohesion(),0.0000001);
+        assertEquals(6f / 10f, ckClass.getLooseClassCohesion(),0.0000001);
     }
 
     @Test
     public void hugeClassCohesion(){
         Map<String, CKClassResult> reportHuge = run(fixturesDir() + "/real-world-huge-class");
         CKClassResult ckClass = reportHuge.get("com.satoshilabs.trezor.lib.protobuf.TrezorMessage");
-        Assert.assertTrue(ckClass.getLooseClassCohesion() < 0.000001f);
+        assertTrue(ckClass.getLooseClassCohesion() < 0.000001f);
     }
 
     @Test
     public void noVisibleMethods(){
         CKClassResult ckClass = report.get("ClassCohesion.NoVisibleMethods");
-        Assert.assertEquals(-1, ckClass.getLooseClassCohesion(),0.0000001);
+        assertEquals(-1, ckClass.getLooseClassCohesion(),0.0000001);
     }
 }
--- src/test/java/com/github/mauricioaniche/ck/VisibleMethodsTest.java
+++ src/test/java/com/github/mauricioaniche/ck/VisibleMethodsTest.java
@@ -1,12 +1,13 @@
 package com.github.mauricioaniche.ck;
 
 import com.google.common.collect.Sets;
-import junit.framework.Assert;
+
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 
-import java.util.Map;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -21,19 +22,19 @@ public class VisibleMethodsTest extends BaseTest{
     @Test
     public void countVisible() {
         CKClassResult a = report.get("VisibleMethods.VisibleMethods");
-        Assert.assertEquals(8, a.getVisibleMethods().size());
+        assertEquals(8, a.getVisibleMethods().size());
     }
 
     @Test
     public void countNonVisible() {
         CKClassResult a = report.get("VisibleMethods.VisibleMethods");
-        Assert.assertEquals(4, a.getNumberOfMethods() - a.getNumberOfVisibleMethods());
+        assertEquals(4, a.getNumberOfMethods() - a.getNumberOfVisibleMethods());
     }
 
     @Test
     public void visibleMethods() {
         CKClassResult a = report.get("VisibleMethods.VisibleMethods");
         Set<String> visibleMethodsFound = a.getVisibleMethods().stream().map(method -> method.getMethodName()).collect(Collectors.toSet());
-        Assert.assertEquals(Sets.newHashSet("m3/0", "m4/0", "m6/0", "m7/0", "m8/0", "m10/0", "m11/0", "m12/0"), visibleMethodsFound);
+        assertEquals(Sets.newHashSet("m3/0", "m4/0", "m6/0", "m7/0", "m8/0", "m10/0", "m11/0", "m12/0"), visibleMethodsFound);
     }
 }
