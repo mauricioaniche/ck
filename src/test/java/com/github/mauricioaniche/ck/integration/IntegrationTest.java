0158ad71652f1e51f10f8344eaca0707d3038b39
David van der Leij <davidvanderleij@gmail.com>
2020-09-09 18:33:23 +0200

    Improve test so that it doesn't perform the same clone multiple times

--- src/test/java/com/github/mauricioaniche/ck/integration/IntegrationTest.java
+++ src/test/java/com/github/mauricioaniche/ck/integration/IntegrationTest.java
@@ -1,25 +1,29 @@
 package com.github.mauricioaniche.ck.integration;
 
-import com.github.mauricioaniche.ck.CK;
-import com.github.mauricioaniche.ck.CKClassResult;
-import com.github.mauricioaniche.ck.CKNotifier;
-import com.google.common.io.Files;
-import org.apache.commons.io.FileUtils;
-import org.eclipse.jgit.api.Git;
-import org.eclipse.jgit.api.errors.GitAPIException;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
+import static com.github.mauricioaniche.ck.AssertResult.assertResultNotNull;
 
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.stream.Stream;
 
-import static com.github.mauricioaniche.ck.AssertResult.assertResultNotNull;
+import com.github.mauricioaniche.ck.CK;
+import com.github.mauricioaniche.ck.CKClassResult;
+import com.github.mauricioaniche.ck.CKNotifier;
+import com.google.common.io.Files;
+
+import org.apache.commons.io.FileUtils;
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
 
 /**
  * These tests mostly look for crashes ("notifyError") or for objects that come
@@ -27,67 +31,71 @@ import static com.github.mauricioaniche.ck.AssertResult.assertResultNotNull;
  *
  * They require internet connection and might take a while to run.
  */
-public class IntegrationTest {
+@TestInstance(Lifecycle.PER_CLASS)
+class IntegrationTest {
 
-    private final String tempFolder = Files.createTempDir().getAbsolutePath();
+    private final static String APACHE_COMMONS_LANG_GIT_URL = "https://github.com/apache/commons-lang.git";
 
-    @AfterEach
-    void deleteTempDir() throws IOException {
-        FileUtils.deleteDirectory(new File(this.tempFolder));
+    private static final String[] REPO_URLS = new String[] { APACHE_COMMONS_LANG_GIT_URL,
+            "https://github.com/apache/commons-math.git" };
+
+    private final Map<String, String> urlTmpDirMap = new HashMap<>();
+
+    private final CKNotifier ckNotifier = new CKNotifier() {
+        @Override
+        public void notify(CKClassResult result) {
+            assertResultNotNull(result);
+        }
+
+        @Override
+        public void notifyError(String sourceFilePath, Exception e) {
+            Assertions.fail(sourceFilePath, e);
+        }
+    };
+
+    private final CK ck = new CK();
+
+    @BeforeAll
+    void cloneRepos() throws GitAPIException {
+        for (String url : REPO_URLS) {
+            String dstDir = Files.createTempDir().getAbsolutePath();
+            clone(url, dstDir);
+            urlTmpDirMap.put(url, dstDir);
+        }
     }
 
-    @ParameterizedTest
-    @CsvSource({ "https://github.com/apache/commons-lang.git", "https://github.com/apache/commons-math.git" })
-    void checkForCrashes(String uri) throws Exception {
-        clone(uri);
+    @AfterAll
+    void deleteTempDir() throws IOException {
+        for (String tmpDir : urlTmpDirMap.values()) {
+            FileUtils.deleteDirectory(new File(tmpDir));
+        }
+    }
 
-        new CK().calculate(tempFolder, new CKNotifier() {
-            @Override
-            public void notify(CKClassResult result) {
-                assertResultNotNull(result);
-            }
-
-            @Override
-            public void notifyError(String sourceFilePath, Exception e) {
-                Assertions.fail(sourceFilePath, e);
-            }
-        });
+    @Test
+    void checkForCrashes() throws Exception {
+        for (String repoFolder : urlTmpDirMap.values()) {
+            ck.calculate(repoFolder, ckNotifier);
+        }
     }
 
     @Test
     void testIndividualFiles() throws GitAPIException {
-        clone("https://github.com/apache/commons-lang.git");
-        Path tempFolderPath = Paths.get(tempFolder);
+        Path tempFolderPath = Paths.get(urlTmpDirMap.get(APACHE_COMMONS_LANG_GIT_URL));
         String classFolder = "src/main/java/org/apache/commons/lang3/";
 
         String[] inputFiles = new String[] { "CharEncoding.java", "RegExUtils.java", "SystemUtils.java",
                 "ThreadUtils.java" };
 
         String[] absInputPaths = Stream.of(inputFiles)
-                .map(inputFile -> tempFolderPath
-                    .resolve(classFolder)
-                    .resolve(inputFile)
-                    .toString()
-                )
+                .map(inputFile -> tempFolderPath.resolve(classFolder).resolve(inputFile).toString())
                 .toArray(String[]::new);
 
-        new CK().calculate(tempFolder, new CKNotifier() {
-            @Override
-            public void notify(CKClassResult result) {
-                assertResultNotNull(result);
-            }
-
-            @Override
-            public void notifyError(String sourceFilePath, Exception e) {
-                Assertions.fail(sourceFilePath, e);
-            }
-        }, absInputPaths);
+        ck.calculate(tempFolderPath.toString(), ckNotifier, absInputPaths);
     }
 
-    private void clone(String uri) throws GitAPIException {
-        try (Git git = Git.cloneRepository().setDirectory(new File(tempFolder)).setURI(uri).setCloneAllBranches(true)
+    private void clone(String uri, String dstDir) throws GitAPIException {
+        try (Git git = Git.cloneRepository().setDirectory(new File(dstDir)).setURI(uri).setCloneAllBranches(true)
                 .call()) {
         }
-        ;
     }
 }
