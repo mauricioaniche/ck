690e915a597b4064aa09c6b474bfe1bc1cd79388
ali taheri nastooh <ali.taherinastooh@gmail.com>
2021-02-14 17:12:36 +0330

    add numberOfParenthesis test:

---
+++ fixtures/parenthesis/NumberOfParenthesis.java
@@ -0,0 +1,15 @@
+package cbo;
+
+public class NumberOfParenthesis {
+
+    private int c = 4;
+    private int d = c * 4;
+
+    public D m1() {
+        int a = 0;
+        int b = 2;
+        a = 2+((b*2)/3);
+
+        return a;
+    }
+}
