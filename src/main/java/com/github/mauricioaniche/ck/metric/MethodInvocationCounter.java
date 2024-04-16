9fb521ec29b27647a8bd3ac7d70147a345bcef10
Breno Farias da Silva <breno.vicio78@gmail.com>
2024-04-16 16:03:36 -0300

    FEAT: Adding the MethodInvocationCounter Class Metric

--- src/main/java/com/github/mauricioaniche/ck/metric/MethodInvocationCounter.java
+++ src/main/java/com/github/mauricioaniche/ck/metric/MethodInvocationCounter.java
@@ -2,29 +2,37 @@ package com.github.mauricioaniche.ck.metric;
 
 import com.github.mauricioaniche.ck.CKClassResult;
 import com.github.mauricioaniche.ck.util.MethodCounter;
-import org.eclipse.jdt.core.dom.MethodInvocation;
+import org.eclipse.jdt.core.dom.*;
+
+import java.util.*;
 
 public class MethodInvocationCounter implements CKASTVisitor, ClassLevelMetric {
-    private String methodList;
+    private Map<String, MethodCounter.MethodInformation> methodInvocations = new HashMap<>();
+    private String currentMethod = null;
+
+    @Override
+    public void visit(MethodDeclaration node) {
+        // Set the current method context
+        this.currentMethod = node.getName().getIdentifier();
+        methodInvocations.putIfAbsent(currentMethod, new MethodCounter().new MethodInformation(currentMethod));
+    }
 
     @Override
     public void visit(MethodInvocation node) {
-        if (methodList == null) {
-            this.methodList = "";
+        if (currentMethod != null) {
+            // Retrieve or create the MethodInformation for the current method
+            MethodCounter.MethodInformation info = methodInvocations.get(currentMethod);
+            String methodName = node.getName().getIdentifier();
+            Map<String, Integer> counts = info.getMethodInvocations();
+            counts.put(methodName, counts.getOrDefault(methodName, 0) + 1);
         }
-
-        String methodName = node.toString();
-
-        if (methodName.contains("(")) {
-            methodName = methodName.substring(0, methodName.indexOf("("));
-        }
-
-        this.methodList += methodName + ";";
     }
 
     @Override
     public void setResult(CKClassResult result) {
-        String methodList = MethodCounter.formatResult(MethodCounter.sort(MethodCounter.count(this.methodList)));
-        result.setMethodInvocation(methodList);
+        // Process all collected method invocation information into the desired format
+        List<MethodCounter.MethodInformation> infos = new ArrayList<>(methodInvocations.values());
+        String formattedResult = MethodCounter.formatResult(infos);
+        result.setMethodInvocation(formattedResult);
     }
 }
